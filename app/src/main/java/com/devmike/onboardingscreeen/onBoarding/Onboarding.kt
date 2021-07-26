d473b8da82e0005a3ffc14ae9d9ae35854102f4e
ndiritumichael <ndiritumichael5@gmail.com>
2021-07-26 23:52:03 +0300

    Implemented the bottom Section that has the indicators and  page Button

--- app/src/main/java/com/devmike/onboardingscreeen/onBoarding/Onboarding.kt
+++ app/src/main/java/com/devmike/onboardingscreeen/onBoarding/Onboarding.kt
@@ -5,13 +5,17 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowLeft
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -46,14 +50,69 @@ fun TopSection(){
 }
 
 @Composable
-@Preview
-fun Indicator(){
-    val isSelected = true
+
+fun BottomSection(size:Int,
+                  index:Int,
+onNextClicked : ()-> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+    ){
+//indicators
+        Indicators(size = size, index =index )
+
+
+//next button
+
+        FloatingActionButton(onClick = onNextClicked,
+        modifier = Modifier.align(Alignment.CenterEnd),
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = MaterialTheme.colors.onPrimary) {
+            Icon(Icons.Outlined.KeyboardArrowRight, contentDescription = "next")
+
+        }
+
+
+
+
+    }
+}
+
+@Composable
+fun BoxScope.Indicators(size : Int,index:Int){
+
+    Row (
+        verticalAlignment =  Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier.align(Alignment.CenterStart)
+    ){
+        repeat(size){
+            Indicator(isSelected  = it == index)
+        }
+
+    }
+}
+
+@Composable
+//@PreviewParameter()
+fun Indicator(isSelected:Boolean){
+
     val width = animateDpAsState(targetValue = if (isSelected) 25.dp else 10.dp,
     animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy))
-    Box(modifier = Modifier.height(1.dp)
-        .background(if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onBackground.copy(alpha = 0.5f))
-
+    Box(modifier = Modifier
+        .height(10.dp)
+        .width(width.value)
+        .clip(CircleShape)
+        .background(
+            if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onBackground.copy(
+                alpha = 0.5f
+            )
         )
+
+        ){
+
+
+    }
 
 }

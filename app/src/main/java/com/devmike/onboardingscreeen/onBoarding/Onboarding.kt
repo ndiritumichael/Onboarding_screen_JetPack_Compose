74cfb7e55877e0b0557dcb0e08ba0f0a13e844cc
ndiritumichael <ndiritumichael5@gmail.com>
2021-07-26 23:11:34 +0300

    Adding Page Indicators

--- app/src/main/java/com/devmike/onboardingscreeen/onBoarding/Onboarding.kt
+++ app/src/main/java/com/devmike/onboardingscreeen/onBoarding/Onboarding.kt
@@ -1,9 +1,10 @@
 package com.devmike.onboardingscreeen.onBoarding
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.absoluteOffset
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowLeft
@@ -26,19 +27,33 @@ fun TopSection(){
         .fillMaxWidth()
         .padding(12.dp)){
         
-        
+        //back button
         IconButton(onClick = { /*TODO*/ },
         modifier = Modifier.align(Alignment.CenterStart)) {
             Icon(Icons.Outlined.KeyboardArrowLeft,null)
             
         }
-        
+
+        //skip test button
         TextButton(onClick = { /*TODO*/ },
         modifier = Modifier.align(Alignment.CenterEnd)) {
-            Text(text = "Skip",color = MaterialTheme.colors.background)
+            Text(text = "Skip",color = MaterialTheme.colors.onBackground)
         }
     }
     
     
     
+}
+
+@Composable
+@Preview
+fun Indicator(){
+    val isSelected = true
+    val width = animateDpAsState(targetValue = if (isSelected) 25.dp else 10.dp,
+    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy))
+    Box(modifier = Modifier.height(1.dp)
+        .background(if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onBackground.copy(alpha = 0.5f))
+
+        )
+
 }

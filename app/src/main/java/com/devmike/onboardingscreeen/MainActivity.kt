691d36afafe928fedad1084197614961e427be43
ndiritumichael <ndiritumichael5@gmail.com>
2021-07-27 21:40:07 +0300

    Clean up and refactoring

--- app/src/main/java/com/devmike/onboardingscreeen/MainActivity.kt
+++ app/src/main/java/com/devmike/onboardingscreeen/MainActivity.kt
@@ -8,16 +8,21 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.devmike.onboardingscreeen.onBoarding.OnBoarding
 import com.devmike.onboardingscreeen.ui.theme.OnboardingScreeenTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+
 //https://dribbble.com/shots/13569004-Onboarding
 class MainActivity : ComponentActivity() {
+    @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OnboardingScreeenTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                  //  Greeting("Android")
+                    OnBoarding()
                 }
             }
         }
